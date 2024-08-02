4e8fddbf247d1185aff16a1e535976777d7535cf
Berenice Huerta <berenice_huerta@epam.com>
2024-08-02 15:14:05 -0600

    deleted non used tests

--- src/tests/test-2.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.epam.com/');
-  await page.getByRole('button', { name: 'Accept All' }).click();
-  await page.locator('.hamburger-menu__button').click();
-  await page.locator('span').filter({ hasText: 'About About' }).getByRole('link').click();
-  await page.locator('body').click();
-});
